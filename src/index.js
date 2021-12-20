0c0812b2d7edb65027db1e0cfb81ec6e95d78e52
Leandro Ferreira de Jesus <leandro.ferreira@4mapit.com>
2021-12-20 19:32:24 -0300

    Criando pagina de consulta

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const multer = require('multer')
+const axios = require('axios');
+
+const upload = multer()
+const app = express()
+
+app.use(express.json())
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
+
+app.get('/consulta', (req, res) => {
+    res.render('pages/consulta', {dados: [] ,trackingcode: ''})
+})
+
+app.get('/consulta-envio', (req, res) => {
+
+    axios.get(`http://lis.leega.com.br/Documento/${req.query.trackingcode}/ObterInfo`).
+        then((response) => {
+            res.render('pages/consulta', {
+                dados: response.data,
+                trackingcode: req.query.trackingcode
+            })
+        })
+
+})
+
+app.get('/envio', (req, res) => {
+    res.render('pages/envio')
+})
+
+app.post('/upload', upload.single('avatar'), (req, res) => {
+    console.log(req.file)
+})
+
+const PORT = 5000 || process.env.PORT
+app.listen(PORT, () => console.log(`Rodando em http://localhost:${PORT}`))
