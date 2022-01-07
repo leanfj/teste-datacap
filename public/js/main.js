f3916304816045e3f407ab69e934db58f04a1c1a
leanfj <leanfj@hotmail.com>
2022-01-07 18:35:58 -0300

    Atualização de functions

--- public/js/main.js
+++ public/js/main.js
@@ -10,17 +10,23 @@ window.addEventListener('load', () => {
         a.href = `/consulta-envio?trackingcode=${data}`
         result.appendChild(a)
     }
+
+    const handlerInputElement = (inputElement) => {
+        const fileInput = document.getElementById(inputElement)
+        const formData = new FormData()
+
+        formData.append('file', fileInput.files[0])
+
+        return formData
+
+    }
     
     if (window.location.pathname === '/envio') {
         document.getElementById('formWaterFile').addEventListener('submit', (e) => {
             e.preventDefault()
             
-            const fileInput = document.getElementById('inputWaterFile')
-            const formData = new FormData()
-    
-            formData.append('file', fileInput.files[0])
-    
-            axios.post('http://lis.leega.com.br/Emissor/3/TipoDocumento/3/Upload/1', formData, {
+  
+            axios.post('http://lis.leega.com.br/Emissor/3/TipoDocumento/3/Upload/1', handlerInputElement('inputWaterFile'), {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
@@ -31,15 +37,11 @@ window.addEventListener('load', () => {
             )
     
         })
+
         document.getElementById('formEletroFile').addEventListener('submit', (e) => {
             e.preventDefault()
-            
-            const fileInput = document.getElementById('inputEletroFile')
-            const formData = new FormData()
     
-            formData.append('file', fileInput.files[0])
-    
-            axios.post('http://lis.leega.com.br/Emissor/2/TipoDocumento/2/Upload/1', formData, {
+            axios.post('http://lis.leega.com.br/Emissor/2/TipoDocumento/2/Upload/1', handlerInputElement('inputEletroFile'), {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
@@ -50,15 +52,11 @@ window.addEventListener('load', () => {
             )
     
         })
+
         document.getElementById('formServiceFile').addEventListener('submit', (e) => {
             e.preventDefault()
             
-            const fileInput = document.getElementById('inputServiceFile')
-            const formData = new FormData()
-    
-            formData.append('file', fileInput.files[0])
-    
-            axios.post('http://lis.leega.com.br/Emissor/1/TipoDocumento/5/Upload/1', formData, {
+            axios.post('http://lis.leega.com.br/Emissor/1/TipoDocumento/5/Upload/1', handlerInputElement('inputServiceFile'), {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
